8c468941a4607ea1cce4877ac1756686857415ec
Johnathon Henderson <sweetjohn33@gmail.com>
2019-05-06 17:25:10 -0700

    small fixes

--- Rating-Systems/game_handler.py
+++ Rating-Systems/game_handler.py
@@ -1,6 +1,20 @@
 from numpy import random
 from player import Player
 from itertools import combinations
+import time
+
+
+def timeit(f):
+    def helper(*args):
+        start = time.time()
+        result = f(*args)
+        finish = time.time()
+        print("It took {} seconds to process.".format(finish-start))
+        return result
+    return helper
+
+
+
 
 
 class GameHandler:
@@ -9,16 +23,17 @@ class GameHandler:
         self.players = players
         self.K = 32
 
+    @timeit
     def round_robin(self, num_rounds):
-        for num in num_rounds:
+        for i in range(num_rounds):
             for pair in combinations(self.players, 2):
                 self.one_game(pair[0], pair[1])
         self.print_standings()
 
     def print_standings(self):
-        self.players.sort(key=lambda x: x.elo)
+        ## self.players.sort(key=lambda x: x.elo)
         for player in self.players:
-            print("{} : {}".format(player.name, player.elo))
+            print("{} : {}".format(player.name, round(player.elo, 0)))
 
     def one_game(self, player1, player2):
         p1_score = player1.get_score()
@@ -31,7 +46,7 @@ class GameHandler:
             self.adjust_elo(player1, player2, True)
 
     def adjust_elo(self, winner, loser, tie=False):
-        winner_expected = 1 / (1 + 10 ^ ((loser.elo - winner.elo) / 400))
+        winner_expected = 1 / (1 + 10 ** ((loser.elo - winner.elo) / 400))
         loser_expected = 1 - winner_expected
         if not tie:
             winner.update_elo(self.K * (1 - winner_expected))
@@ -40,9 +55,9 @@ class GameHandler:
             winner.update_elo(self.K * (0.5 - winner_expected))
             loser.update_elo(self.K * (0.5 - loser_expected))
 
-
-
-
+    def reset(self):
+        for player in self.players:
+            player.elo = 1000
 
 
 
--- Rating-Systems/simulate.py
+++ Rating-Systems/simulate.py
@@ -1,13 +1,22 @@
 from game_handler import GameHandler
 from player import Player
 
-jill = Player("Jill", 50, 10)
-jack = Player("Jack", 60, 20)
-hansel = Player("Hansel", 40, 30)
-gretel = Player("Gretel", 50, 40)
+jill = Player("Jill", 50, 5)
+jack = Player("Jack", 50, 10)
+hansel = Player("Hansel", 50, 15)
+gretel = Player("Gretel", 50, 20)
 
 game = GameHandler([jill, jack, hansel, gretel])
 
-game.round_robin(3)
+## for i in range(3):
+
+game.round_robin(100000)
+game.reset()
+game.round_robin(100000)
+game.reset()
+game.round_robin(100000)
+game.reset()
+
+
 
 
